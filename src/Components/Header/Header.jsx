e4eafc93dbd273d1ea5878f589ecbd0f011e85fa
mujahidraj <mujahidraj65@gmail.com>
2025-12-13 18:30:53 +0600

    using the date fns

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from '../../assets/logo.png'
-import './header.css'
+import { format } from 'date-fns';
 
 const Header = () => {
   return (
@@ -8,6 +8,11 @@ const Header = () => {
       <div className='flex flex-col poppins justify-center items-center gap-3 '>
         <img className='w-1/3' src={logo} alt="" />
         <h2 className='text-[#706F6F] text-lg font-medium '>Journalism Without Fear or Favour</h2>
+        <p>
+          {
+            format(new Date(), 'EEEE , MMMM MM , yyyy')
+          }
+        </p>
       </div>
     </div>
   );
