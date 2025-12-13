cd622a8629ae573a022bdc4cb7f7e348a7e0f56f
mujahidraj <mujahidraj65@gmail.com>
2025-12-14 03:09:12 +0600

    homepage resolved

--- src/Components/SocialLogin/SocialLogin.jsx
+++ src/Components/SocialLogin/SocialLogin.jsx
@@ -10,13 +10,13 @@ const SocialLogin = () => {
       <h2 className='font-semibold text-lg '>Login With</h2>
       <div className='flex flex-col gap-3 my-3'>
         {/* Google */}
-        <button className="btn bg-white text-black border-[#e5e5e5]">
+        <button className="btn bg-white text-black w-11/12 border-[#e5e5e5]">
        <FcGoogle size={24} />
 
           Login with Google
         </button>
         {/* GitHub */}
-        <button className="btn bg-black text-white border-black">
+        <button className="btn bg-black text-white w-11/12 border-black">
          <FaGithubSquare size={24}/>
 
           Login with GitHub
--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Category from '../Category/Category';
 import NewsCard from '../../Components/NewsCard/NewsCard';
 import logo from '../../assets/11.png'
 import { FaSmile } from 'react-icons/fa';
+import { Navigate } from 'react-router';
 
 
 
 const HomePage = () => {
   return (
+    <>
+   <Suspense fallback={<span className="loading loading-bars loading-xl"></span>}>
+    
+    <Navigate to='/category/1'></Navigate>
+   </Suspense>
     <div className='w-9/12  text-center flex flex-col justify-center'>
       <h3 className='text-2xl my-10 font-semibold '>Sorry ! Website is on maintainance. Please choose a Category to see news. Thank you ! </h3>
       <img src={logo} className='rounded-full object-center' alt="" />
     </div>
+    </>
   );
 };
 
