9adbe4df4735c6ec5a94ba4d9d0dd82c770c1670
mujahidraj <mujahidraj65@gmail.com>
2025-12-15 23:21:36 +0600

    firebase authentication with the observer

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { use} from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../../Provider/AuthProvider';
 
 const Register = () => {
+
+  const {createUser ,loading, user}= use(AuthContext)
+
+  if(loading){
+    return <span className="loading loading-bars loading-xs"></span>
+
+  }
+  console.log(user);
+  
+
+  const handleOnSubmit =(event)=>{
+    event.preventDefault()
+    const name = event.target.name.value;
+    const photoUrl = event.target.photoUrl.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    console.log(name , email, photoUrl , password);
+
+    createUser(email,password).then(result=>console.log(result.user)).catch(error=>console.log(error.code)
+    )
+    
+
+  }
+
   return (
     <div className="bg-base-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
         <h2 className='text-center text-2xl font-bold'>Register Account</h2>
-        <fieldset className="fieldset">
+        <form onSubmit={handleOnSubmit} className="fieldset">
           <label className="label">name</label>
-          <input type="text" className="input" placeholder="Name" />
+          <input type="text" className="input" name='name' placeholder="Name" required/>
           <label className="label">Photo URL</label>
-          <input type="text" className="input" placeholder="Photo URL" />
+          <input type="text" className="input" name='photoUrl' required placeholder="Photo URL" />
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" className="input" name='email' required placeholder="Email" />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" className="input" name='password' required placeholder="Password" />
           
-          <button className="btn btn-neutral mt-4">Login</button>
-        </fieldset>
+          <button type='submit' className="btn btn-neutral mt-4">Login</button>
+        </form>
         <p className='my-3 text-center'>Already have an account? <Link className='text-blue-500 underline' to='/auth/login'>Please Sign in</Link></p>
       </div>
     </div>
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,17 +1,44 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../Firebase/Firebase.config';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
+
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
 
-  const [user , setUser] = useState({
-    name:"raj",
-    email: "mujahidraj@gmail.com"
-  })
+  const [user , setUser] = useState(null)
+
+  const [loading , setLoading] = useState(true)
+
+  const auth = getAuth(app);
+
+  const createUser =(email , password)=>{
+    setLoading(true)
+    return createUserWithEmailAndPassword(auth , email , password)
+  };
+
+
+  useEffect(()=>{
+    
+    const unSubscribe = onAuthStateChanged (auth , (currentUser)=>{
+      setUser(currentUser)
+      setLoading(false)
+    });
+    return ()=>{
+      unSubscribe();
+    }
+  },[auth])
+
+  
+
+
 
   const authData = {
-    user , setUser
+    user,
+    loading,
+    createUser
   }
 
 
